92fd6c1806a1c3d8ca15e9b3b588d362bd888d6d
Ben Toosey <benjamin.toosey@gmail.com>
2022-07-15 17:42:27 +0100

    ADD: Created Entry class and pushed each new entry object into an entries array for filtering

---
+++ entry.rb
@@ -0,0 +1,8 @@
+class Entry
+  def initialize(rank, title, score, comments)
+    @rank = rank
+    @title = title
+    @score = score
+    @comments = comments
+  end
+end
--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,26 @@
 require 'httparty'
 require 'nokogiri'
 require 'byebug'
+require_relative 'entry'
 
 def scraper
   url = 'https://news.ycombinator.com/'
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page.body)
 
+  entries = []
+
   parsed_page.css('tr.athing').each do |entry|
     subtext = entry.next_element.css('td.subtext')
-    entry = {
-      rank: extract_number(entry.css('span.rank').text),
-      title: entry.css('a.titlelink').text,
-      score: extract_number(subtext.css('span.score').text),
-      comments: extract_number(subtext.css('a').last.text)
-    }
 
-    puts entry
+    rank = extract_number(entry.css('span.rank').text)
+    title = entry.css('a.titlelink').text
+    score = extract_number(subtext.css('span.score').text)
+    comments = extract_number(subtext.css('a').last.text)
+
+    entry = Entry.new(rank, title, score, comments)
+
+    entries << entry
   end
 end
 
