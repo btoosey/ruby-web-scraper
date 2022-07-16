8a41da0e7afddaed44177506316b92ae732a3993
Ben Toosey <benjamin.toosey@gmail.com>
2022-07-16 16:06:36 +0100

    CLEAN: Added comments to code and refactored filter methods

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,12 @@ require 'nokogiri'
 require_relative 'entry'
 
 def scraper
+  entries = []
+
+  # Scrape website for entries
   url = 'https://news.ycombinator.com/'
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page.body)
-
-  entries = []
 
   parsed_page.css('tr.athing').each do |entry|
     subtext = entry.next_element.css('td.subtext')
@@ -22,6 +23,7 @@ def scraper
     entries << entry
   end
 
+  # Display entries to user through required filters
   program_output(entries)
 end
 
@@ -38,17 +40,21 @@ def program_output(entries)
 end
 
 def filter_one(entries)
+  # 1. Select each entry if its title is more than 5 words long
   filtered_entries = more_than_five(entries)
-  ordered_entries = order_entries(filtered_entries, 'comments')
-  ordered_entries.each do |entry|
+
+  # 2. Order by number of comments
+  order_entries(filtered_entries, 'comments').each do |entry|
     puts "#{entry.title} | #{entry.comments} Comments"
   end
 end
 
 def filter_two(entries)
+  # 1. Select each entry if its title is less than or equal to 5 words long
   filtered_entries = less_or_eq_five(entries)
-  ordered_entries = order_entries(filtered_entries, 'score')
-  ordered_entries.each do |entry|
+
+  # 2. Order by score
+  order_entries(filtered_entries, 'score').each do |entry|
     puts "#{entry.title} | #{entry.score} Points"
   end
 end
@@ -70,6 +76,7 @@ def less_or_eq_five(entries)
 end
 
 def order_entries(entries, query)
+  # Sort entries by either comments, or score
   case query
   when 'comments'
     entries.sort_by { |entry| entry.comments }
